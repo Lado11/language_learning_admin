0eb26969162e94bb8d00f8a77ea0a783d259b137
Andranik Metsoyan <andranik.metsoyan@zangi.com>
2024-06-25 18:15:52 +0400

    (AM) get file url

--- src/screens/category-screen/category-screen.js
+++ src/screens/category-screen/category-screen.js
@@ -1,75 +1,91 @@
-import { useNavigate } from "react-router-dom";
-import {
-  CustomCardItem,
-  CustomNoData,
-  CustomPagination,
-} from "../../components";
-import { CustomAddNew } from "../../components/custom-add-new/custom-add-new";
-import "./category-screen.css";
-import { useDispatch, useSelector } from "react-redux";
-import { categoryGetThunk, getCategoryGetData, getCategoryGetLoading } from "../../store/slices/category/get-category";
-import { useEffect } from "react";
-import { CustomSpin } from "../../components/custom-spin/custom-spin";
-import { page0, page12 } from "../../constants/constants";
-import { ConstPagiantion } from "../../constants/const-pagination";
-import { filesGetIdThunk, getfilesGetIdResponse } from "../../store/slices/files/get-id-files";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { categoryGetThunk, getCategoryGetData, getCategoryGetLoading } from '../../store/slices/category/get-category';
+import { filesGetIdThunk, getfilesGetIdResponse } from '../../store/slices/files/get-id-files';
+import { CustomAddNew, CustomCardItem, CustomNoData, CustomPagination, CustomSpin } from '../../components';
+import { ConstPagiantion } from '../../constants/const-pagination';
+import { page0, page6 } from '../../constants/constants';
+import './category-screen.css';
 
 export const CategoryScreen = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const categoryLoading = useSelector(getCategoryGetLoading);
-  const categoryData = useSelector(getCategoryGetData)?.data?.list;
-const catgeoryImageResponse = useSelector(getfilesGetIdResponse)
-console.log(catgeoryImageResponse,"image resposne");
+  const categoryData = useSelector(getCategoryGetData);
+  const [imageUrls, setImageUrls] = useState({});
+
   useEffect(() => {
-    dispatch(categoryGetThunk(ConstPagiantion(page0,page12)));
-  }, [])
+    dispatch(categoryGetThunk(ConstPagiantion(page0, page6)));
+  }, [dispatch]);
+
+  useEffect(() => {
+    // Preload image URLs
+    if (categoryData?.data?.list?.length) {
+      categoryData.data.list.forEach((item) => {
+        fetchImage(item.imageFile);
+      });
+    }
+  }, [categoryData]);
 
   const categoryUpdate = (id) => {
-
-    console.log(id,"d");
-    localStorage.setItem("categoryId",id,)
+    localStorage.setItem('categoryId', id);
     navigate(`/category/${id}`);
   };
+
+  const fetchImage = (imageFileId) => {
+    if (!imageUrls[imageFileId]) {
+      dispatch(filesGetIdThunk(imageFileId));
+    }
+  };
+
+  const categoryImageResponse = useSelector(getfilesGetIdResponse);
+
+  useEffect(() => {
+    // Update imageUrls state with fetched image URLs
+    if (categoryImageResponse?.data?.url) {
+      setImageUrls((prevUrls) => ({
+        ...prevUrls,
+        [categoryImageResponse.data.fileId]: categoryImageResponse.data.url,
+      }));
+    }
+  }, [categoryImageResponse]);
 
   return (
     <div className="nativeLanguageScreenMainDiv">
       <div>
-      <CustomAddNew
-        title="Add New Category"
-        onClick={() => {
-          navigate("/category-create");
-        }}
-      />
-      <p className="category-title">Category</p>
-      <div className="category-item-pagination">
-        {!categoryData?.length && !categoryLoading ? <CustomNoData />:
-       <>
-       {categoryLoading ? <div className="nativeLanguageScreenMainDiv"> <CustomSpin size={64} color="gray" />
-         </div> :
-         <div className="custom-card-item">
-          {categoryData?.map((countryItem, index) => {
-            // dispatch(filesGetIdThunk(countryItem?.imageFile))
-            return (
-              <div className="pointer" key={index + 1}  onClick={()=>{
-                categoryUpdate(countryItem?._id)
-              }}>
-                <CustomCardItem
-                  icon={countryItem?.imageFile?.path}
-                  title={countryItem.name}
-                />
+        <CustomAddNew
+          title="Add New Category"
+          onClick={() => {
+            navigate('/category-create');
+          }}
+        />
+        <p className="category-title">Category</p>
+        <div className="category-item-pagination">
+          {!categoryData?.data?.list?.length && !categoryLoading ? (
+            <CustomNoData />
+          ) : (
+            <>
+              {categoryLoading ? (
+                <div className="nativeLanguageScreenMainDiv">
+                  <CustomSpin size={64} color="gray" />
+                </div>
+              ) : (
+                <div className="custom-card-item">
+                  {categoryData?.data?.list?.map((countryItem, index) => (
+                    <div className="pointer" key={index + 1} onClick={() => categoryUpdate(countryItem._id)}>
+                      <CustomCardItem icon={imageUrls[countryItem.imageFile]} title={countryItem.name} />
+                    </div>
+                  ))}
+                </div>
+              )}
+              <div className="category-pagination">
+                <CustomPagination length={categoryData?.data?.total} pageLength={page6} />
               </div>
-            );
-          })}
-        </div>}
-        <div className="category-pagination">
-          <CustomPagination length={categoryData?.length} pageLength={page12}/>
-        </div>
-       </>
-        }
+            </>
+          )}
         </div>
       </div>
-     
     </div>
   );
 };
